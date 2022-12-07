6f268ead61ccc1ebc92fbdd88e2b7710ccdc5bfd
Surya Kasturi <suryateja516101@gmail.com>
2022-12-07 22:34:53 +0000

    Docker file permission added

--- docker-installation.sh
+++ docker-installation.sh
@@ -14,4 +14,9 @@ sudo apt-get update -y
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
-sudo docker run hello-world
+# Setting permissions for docker
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
+su -s ${USER}
+
+docker run hello-world
