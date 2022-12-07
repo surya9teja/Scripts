a0603e1dc3aa92627632a205e39477148cbd8044
Surya Kasturi <suryateja516101@gmail.com>
2022-12-07 22:31:16 +0000

    made changes to the docker running script

--- oracle-BOA-portal-env-setup.sh
+++ oracle-BOA-portal-env-setup.sh
@@ -57,15 +57,6 @@ cd ~
 echo ""
 echo "Installing Laravel"
 
-docker info > /dev/null 2>&1
-
-# Ensure that Docker is running...
-if [ $? -ne 0 ]; then
-    echo "Docker is not running."
-
-    exit 1
-fi
-
 # Install Laravel with Sail... (https://laravel.com/docs/9.x#getting-started-on-linux)
 docker run --rm \
     --pull=always \
